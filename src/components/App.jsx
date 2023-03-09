de5dffb4f54090e011de52c3e08f9fc8af413d74
Alejandro R <alejorp195@hotmail.com>
2023-03-09 08:37:20 -0500

    Estilos agregados

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ import Img from "./Img";
 
 function App() {
   const [isLoading,setIsLoading] = useState(true); 
+  const [isLoading1,setIsLoading1] = useState(true); 
+  const [isLoading2,setIsLoading2] = useState(true); 
   const [imgUrl1, setImgUrl1] = useState(null);
   const [imgUrl2, setImgUrl2] = useState(null);
   const [imgUrl3, setImgUrl3] = useState(null);
@@ -16,20 +18,25 @@ function App() {
       .then(res => res.json())
       .then(data => {
         setImgUrl1(data[0].url);
-        setIsLoading(false);
+        setIsLoading1(false);
       });
       fetch(APIurl)  
       .then(res => res.json())
       .then(data => {
         setImgUrl2(data[0].url);
-        setIsLoading(false);
+        setIsLoading2(false);
       });
       fetch(APIurl)  
       .then(res => res.json())
       .then(data => {
         setImgUrl3(data[0].url);
-        setIsLoading(false);
+        setIsLoading3(false);
       });
+  }
+
+  const Loading = () => {
+    setIsLoading2(true);
+    setIsLoading1(true);
   }
 
   useEffect(() => {
@@ -37,26 +44,32 @@ function App() {
   }, [])
 
   const updateImg = () => {
-    setIsLoading(true);
+    Loading();
     Fetchh();
   }
   
-  if(isLoading){
+  if(isLoading1 || isLoading2){
     return(
 
       <div className="App">
-        <h1>Cargando Pa</h1>
+        <h1>Cargando</h1>
       </div>
     ) 
   }
 
   return (
-    <div className="App">
-      <h1>Perritos Aleatorios</h1>
-      <Img imgUrl = {imgUrl1}/>
-      <Img imgUrl = {imgUrl2}/>
-      <Btn updateImg={updateImg}>Recargar</Btn>
-    </div>
+    <>
+      <div className="App">
+        <section className="randomSection">
+          <h1>Perritos Aleatorios</h1>
+          <div>
+            <Img imgUrl = {imgUrl1}/>
+            <Img imgUrl = {imgUrl2}/>
+          </div>
+          <Btn updateImg={updateImg}>Recargar</Btn>
+        </section>
+      </div>
+    </>
   )
 }
 
